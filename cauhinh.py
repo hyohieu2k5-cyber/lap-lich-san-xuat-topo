8740a48a9debb3ed9771b5fc7d73661a082e0864
hyohieu2k5-cyber <hyohieu2k5@gmail.com>
2026-06-16 07:53:49 +0700

    Update cauhinh.py

--- cauhinh.py
+++ cauhinh.py
@@ -31,3 +31,34 @@ MAIN_BTN_CFG = {
     "relief": "flat", 
     "cursor": "hand2"
 }
+# ======================
+# THÔNG TIN ỨNG DỤNG
+# ======================
+
+APP_NAME = "Quản lý Lập lịch Sản xuất"
+APP_WIDTH = 1100
+APP_HEIGHT = 650
+
+# ======================
+# FONT CHỮ
+# ======================
+
+FONT_HOME = ("Arial", 26, "bold")
+FONT_TITLE = ("Arial", 14, "bold")
+FONT_NORMAL = ("Arial", 13)
+
+CONTACT_INFO = (
+    "Người tạo: Lâm Bảo Ngọc Hiếu\n"
+    "MSSV: 0023410958\n\n"
+    "Đồ án: Cài đặt chương trình Lập lịch sản xuất trong nhà máy, ứng dụng giải thuật sắp xếp Tôpô\n"
+    "Sản phẩm: Phần mềm Lập lịch Sản xuất v1.0\n\n"
+    "Email: 0023410958@student.dthu.edu.vn\n"
+    "Hotline: 0969 8345 18\n"
+)
+
+HOME_TITLE = "PHẦN MỀM LẬP LỊCH SẢN XUẤT V1.0"
+
+HOME_SUBTITLE = (
+    "Giải pháp tối ưu hóa dây chuyền "
+    "dựa trên Sắp xếp Tôpô"
+)
